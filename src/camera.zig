6ccbbd79cff4c8b2bf9b4af366ae6c398efe861d
mdmrk <mariodavo.20@gmail.com>
2025-02-08 13:09:51 +0100

    refactor: camera

--- src/camera.zig
+++ src/camera.zig
@@ -10,7 +10,6 @@ pub const CameraPosition = enum(u32) {
     front_facing = c.SDL_CAMERA_POSITION_FRONT_FACING,
     back_facing = c.SDL_CAMERA_POSITION_BACK_FACING,
 };
-pub const Camera = c.SDL_Camera;
 pub const Surface = c.SDL_Surface;
 pub const PropertiesID = c.SDL_PropertiesID;
 
@@ -53,47 +52,48 @@ pub fn getCameraPosition(instance_id: CameraID) !CameraPosition {
     return @enumFromInt(c.SDL_GetCameraPosition(instance_id));
 }
 
-pub const CameraDevice = struct {
-    ptr: *Camera,
+pub const Camera = struct {
+    ptr: *c.SDL_Camera,
 
     /// Open a video recording device
-    pub fn init(instance_id: CameraID, spec: *const CameraSpec) !CameraDevice {
-        const camera = try errify(c.SDL_OpenCamera(instance_id, spec));
-        return CameraDevice{ .ptr = camera };
+    pub fn open(instance_id: CameraID, spec: *const CameraSpec) !Camera {
+        return Camera{
+            .ptr = try errify(c.SDL_OpenCamera(instance_id, spec)),
+        };
     }
 
     /// Query if camera access has been approved by the user
-    pub fn getPermissionState(self: CameraDevice) !c_int {
+    pub fn getPermissionState(self: *const Camera) !c_int {
         return try errify(c.SDL_GetCameraPermissionState(self.ptr));
     }
 
     /// Get the instance ID of an opened camera
-    pub fn getID(self: CameraDevice) CameraID {
+    pub fn getID(self: *const Camera) CameraID {
         return c.SDL_GetCameraID(self.ptr);
     }
 
     /// Get the properties associated with an opened camera
-    pub fn getProperties(self: CameraDevice) PropertiesID {
+    pub fn getProperties(self: *const Camera) PropertiesID {
         return c.SDL_GetCameraProperties(self.ptr);
     }
 
     /// Get the spec that a camera is using when generating images
-    pub fn getFormat(self: CameraDevice, spec: *CameraSpec) bool {
+    pub fn getFormat(self: *const Camera, spec: *CameraSpec) bool {
         return c.SDL_GetCameraFormat(self.ptr, spec);
     }
 
     /// Acquire a frame
-    pub fn acquireFrame(self: CameraDevice, timestamp_ns: *u64) !*Surface {
+    pub fn acquireFrame(self: *const Camera, timestamp_ns: *u64) !*Surface {
         return try errify(c.SDL_AcquireCameraFrame(self.ptr, timestamp_ns));
     }
 
     /// Release a frame of video acquired from a camera
-    pub fn releaseFrame(self: CameraDevice, frame: *Surface) void {
+    pub fn releaseFrame(self: *const Camera, frame: *Surface) void {
         c.SDL_ReleaseCameraFrame(self.ptr, frame);
     }
 
     /// Shut down camera processing and close the camera device
-    pub fn deinit(self: CameraDevice) void {
+    pub fn close(self: *const Camera) void {
         c.SDL_CloseCamera(self.ptr);
     }
 };

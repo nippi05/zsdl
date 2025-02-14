bfa4ef9a21b780d5e69d0d7ad80c68d0c2a81b14
mdmrk <mariodavo.20@gmail.com>
2025-02-14 19:00:58 +0100

    docs(timer): add top docs

--- src/timer.zig
+++ src/timer.zig
@@ -3,52 +3,56 @@ const internal = @import("internal.zig");
 const c = @import("c.zig").c;
 const errify = internal.errify;
 
-pub const MS_PER_SECOND = c.SDL_MS_PER_SECOND;
-pub const US_PER_SECOND = c.SDL_US_PER_SECOND;
-pub const NS_PER_SECOND = c.SDL_NS_PER_SECOND;
-pub const NS_PER_MS = c.SDL_NS_PER_MS;
-pub const NS_PER_US = c.SDL_NS_PER_US;
-
 pub const TimerID = c.SDL_TimerID;
 pub const TimerCallback = c.SDL_TimerCallback;
 pub const NSTimerCallback = c.SDL_NSTimerCallback;
 
+/// Get the number of milliseconds since SDL library initialization.
 pub fn getTicks() u64 {
     return c.SDL_GetTicks();
 }
 
+/// Get the number of nanoseconds since SDL library initialization.
 pub fn getTicksNS() u64 {
     return c.SDL_GetTicksNS();
 }
 
+/// Get the current value of the high resolution counter.
 pub fn getPerformanceCounter() u64 {
     return c.SDL_GetPerformanceCounter();
 }
 
+/// Get the count per second of the high resolution counter.
 pub fn getPerformanceFrequency() u64 {
     return c.SDL_GetPerformanceFrequency();
 }
 
+/// Wait a specified number of milliseconds before returning.
 pub fn delay(ms: u32) void {
     c.SDL_Delay(ms);
 }
 
+/// Wait a specified number of nanoseconds before returning.
 pub fn delayNS(ns: u64) void {
     c.SDL_DelayNS(ns);
 }
 
+/// Wait a specified number of nanoseconds before returning.
 pub fn delayPrecise(ns: u64) void {
     c.SDL_DelayPrecise(ns);
 }
 
+/// Call a callback function at a future time.
 pub fn addTimer(interval: u32, callback: TimerCallback, userdata: ?*anyopaque) !TimerID {
     return try errify(c.SDL_AddTimer(interval, callback, userdata));
 }
 
+/// Call a callback function at a future time.
 pub fn addTimerNS(interval: u64, callback: NSTimerCallback, userdata: ?*anyopaque) !TimerID {
     return try errify(c.SDL_AddTimerNS(interval, callback, userdata));
 }
 
+/// Remove a timer created with SDL_AddTimer().
 pub fn removeTimer(id: TimerID) !void {
     try errify(c.SDL_RemoveTimer(id));
 }
